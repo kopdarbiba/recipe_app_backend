863b2726b5ec50242611be0df3920c0135b8b522
Tom <tomsterauds@gmailcom>
2023-11-26 10:34:34 +0200

    added sample recipes. fech them with get request

--- api/views.py
+++ api/views.py
@@ -1,12 +1,19 @@
-import json
 from django.http import JsonResponse
-from django.forms import model_to_dict
-from catalog.models import Recipe
+from django.forms.models import model_to_dict
+from django.shortcuts import get_object_or_404
+from catalog.models import Recipe  # Import your Recipe model
 
-def api_home(request, *args, **kwargs):
+def api_home(request):
+    # Example http://127.0.0.1:8000/api/?recipe_id=2
+    recipe_id = request.GET.get('recipe_id')
 
-    model_data = Recipe.objects.all().order_by("?").first()
-    data ={}
-    if model_data:
-        data = model_to_dict(model_data, fields=['id', 'servings'])
+    if recipe_id is not None:
+        try:
+            model_data = get_object_or_404(Recipe, id=recipe_id)
+            data = model_to_dict(model_data)  # You can specify fields=['id', 'instructions', 'servings'] if needed
+        except ValueError:
+            data = {'error': 'Invalid recipe ID'}
+    else:
+        data = {'error': 'Recipe ID not provided in the request'}
+
     return JsonResponse(data)
