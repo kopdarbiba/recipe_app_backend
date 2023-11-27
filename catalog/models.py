387c7f78ea5b8d00fa73aca3b2fc5b0d294c7eb2
Tom <tomsterauds@gmailcom>
2023-11-27 16:30:57 +0200

    marge dat from Anta branch

--- catalog/models.py
+++ catalog/models.py
@@ -3,6 +3,40 @@ from django.db import models
 # Create your models here.
 class Recipe(models.Model):
     title = models.CharField(max_length=150)
-    instructions = models.TextField(blank=True, null=True)
-    servings = models.DecimalField(max_digits=10, decimal_places=2, default=1.00 )
-    
+    instructions = models.TextField(blank=True, null=True) # can be removed later
+    servings = models.IntegerField(default=1)
+
+    # # Basic info
+    # description = models.TextField()
+    # cuisine = models.CharField(max_length=50)
+    # meal = models.CharField(max_length=50)
+    # food_category = models.CharField(max_length=50)
+
+
+    # # Details
+    # cooking_time = models.IntegerField(default=0)
+    # equipment = models.TextField(blank=True, null=True)
+    # ingredient = models.TextField()
+    # cooking_steps = models.TextField()
+    # allergens = models.TextField(blank=True, null=True)
+    # ingredient_substitude = models.TextField(blank=True, null=True)
+
+    # # Nutrition
+    # nutritional_info = models.TextField(blank=True, null=True)
+    # calories = models.DecimalField(max_digits=7, decimal_places=2, default=0.0)
+
+    # # Rating
+    # rating = models.DecimalField(max_digits=3, decimal_places=2, default=0.0)
+
+    # # Additional info
+    # dietary_preference = models.CharField(max_length=50, blank=True, null=True)
+    # measurements = models.DecimalField(max_digits=3, decimal_places=1, default=1)
+
+    # # Date and Time
+    # created_at = models.DateTimeField(auto_now_add=True)
+    # updated_at = models.DateTimeField(auto_now=True)
+
+
+
+
+ 
---
+++ google_sheet.py
@@ -0,0 +1,42 @@
+import gspread
+from catalog.models import Recipe
+
+# Authenticate with Google Sheets
+gc = gspread.service_account(filename='recipeapp.json')
+
+# Open the Google Sheet by name
+sh = gc.open('Kopdarbs')
+
+# Specify the name of the desired sheet
+sheet_name = 'RecipeData'
+
+# Access the specified worksheet
+worksheet = sh.worksheet(sheet_name)
+
+# Get all values in the sheet
+all_values = worksheet.get_all_values()
+
+print(all_values)
+
+# # Define column indices based on the sheet structure
+# column_indices = {
+#     "title": 0,
+#     "instructions": 1,
+#     "servings": 5,
+# }
+
+# # Skip the header row
+# data_rows = all_values[1:]
+
+# # Iterate over the data rows
+# for row_data in data_rows:
+#     # Extract values from the row based on column indices
+#     values = {
+#         "title": row_data[column_indices["title"]],
+#         "instructions": row_data[column_indices["instructions"]],
+#         "servings": float(row_data[column_indices["servings"]]),
+#     }
+
+#     # Create a Recipe instance and save it to the database
+#     recipe = Recipe(**values)
+#     recipe.save()
