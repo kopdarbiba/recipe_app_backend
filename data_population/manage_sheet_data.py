2886440ba3dd54e402c60f19afc4411c5f410edc
Tom <tomsterauds@gmailcom>
2023-11-28 15:55:15 +0200

    db_populate improved

--- data_population/manage_sheet_data.py
+++ data_population/manage_sheet_data.py
@@ -7,36 +7,36 @@ from catalog.models import Recipe
 #         ingredient.save()
 
 def db_populate(sheet_data_dict: dict) -> None:
-    recipe_fields = {
-    'title': 'title',
-    'servings': 'servings',
-    'description': 'description',
-    'cuisine': 'cuisine',
-    'meal': 'meal',
-    'food_category': 'food_category',
-    'cooking_time': 'cooking_time',
-    'equipment': 'equipment',
-    'ingredient': 'ingredient',
-    'cooking_steps': 'cooking_steps',
-    'allergens': 'allergens',
-    'ingredient_substitute': 'ingredient_substitute',
-    'nutritional_information': 'nutritional_information',
-    'calories': 'calories',
-    'rating': 'rating',
-    'dietary_preference': 'dietary_preference',
-    'measurements': 'measurements'
-    }
-
     for row in sheet_data_dict:
+        # Convert empty strings to None in the row dictionary
         for field, value in row.items():
-            if value is '':
+            if value == '':
                 row[field] = None
 
-        recipe = Recipe(title=row['title'], servings=row['servings'], description=row['description'],
-                        cuisine=row['cuisine'], meal=row['meal'], food_category=row['food_category'],
-                        cooking_time=row['cooking_time'], equipment=row['equipment'], ingredient=row['ingredient'],
-                        cooking_steps=row['cooking_steps'], allergens=row['allergens'], ingredient_substitute=row['ingredient_substitute'],
-                        nutritional_information=row['nutritional_information'], calories=row['calories'], rating=row['rating'],
-                        dietary_preference=row['dietary_preference'], measurements=row['measurements']
-                        )
+        # Create a dictionary with cleaned data for creating a Recipe instance
+        recipe_data = {
+            'title': row['title'],
+            'servings': row['servings'],
+            'description': row['description'],
+            'cuisine': row['cuisine'],
+            'meal': row['meal'],
+            'food_category': row['food_category'],
+            'cooking_time': row['cooking_time'],
+            'equipment': row['equipment'],
+            'ingredient': row['ingredient'],
+            'cooking_steps': row['cooking_steps'],
+            'allergens': row['allergens'],
+            'ingredient_substitute': row['ingredient_substitute'],
+            'nutritional_information': row['nutritional_information'],
+            'calories': row['calories'],
+            'rating': row['rating'],
+            'dietary_preference': row['dietary_preference'],
+            'measurements': row['measurements'],
+        }
+
+        # Remove keys with values set to None
+        recipe_data = {k: v for k, v in recipe_data.items() if v is not None}
+
+        # Create a Recipe instance and save it to the database
+        recipe = Recipe(**recipe_data)
         recipe.save()
