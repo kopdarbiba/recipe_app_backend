793f09e18f97c234778d8b2dbce6f9824357762b
Tom <tomsterauds@gmailcom>
2023-12-02 15:06:28 +0200

    ingredient table category_id field update works

--- catalog/management/commands/db_manager.py
+++ catalog/management/commands/db_manager.py
@@ -9,7 +9,7 @@ class Command(BaseCommand):
     help = 'Populate Ingridient objects'
 
     def handle(self, *args, **options):
-        db_table_list = ['unit', 'dietarypreference', 'allergen', 'equipment', 'meal', 'cuisine', 'ingredient', 'ingredient_category'] # Add table names
+        db_table_list = ['unit', 'dietarypreference', 'allergen', 'equipment', 'meal', 'cuisine', 'ingredient_category', 'ingredient' ] # Add table names
         relative_key_path = config('RELATIVE_KEY_PATH')
         
         for table_name in db_table_list:
