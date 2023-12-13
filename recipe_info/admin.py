b9abb070416c70198cee89643e901cdb87aec222
Toms <tomsterauds@gmail.com>
2023-12-13 20:18:45 +0200

    last

--- recipe_info/admin.py
+++ recipe_info/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
-from .models import RecipeGenInfo, RecipeIngredient, Unit, CookingMethod
+from .models import GenInfo, RecipeIngredient, Unit
+from .models import Recipe
+
+# Manage unit type boolean values
+class UnitAdmin(admin.ModelAdmin):
+    list_display = ["name_lv", "type_shoping_valid"]
 
 # Ingredient selector for general info form. Filter, to show only shoping valid units
 class RecipeIngredientInline(admin.TabularInline):
@@ -11,25 +16,22 @@ class RecipeIngredientInline(admin.TabularInline):
             kwargs["queryset"] = Unit.objects.filter(type_shoping_valid=True)
         return super().formfield_for_foreignkey(db_field, request, **kwargs)
 
-# Manage unit type boolean values
-class UnitAdmin(admin.ModelAdmin):
-    list_display = ["name_lv", "type_shoping_valid"]
-
 # Modify general info form, adding ingredient selector at botom
 class RecipeGenInfoAdmin(admin.ModelAdmin):
     inlines = [RecipeIngredientInline]
 
 
 admin.site.register(Unit, UnitAdmin)
-admin.site.register(RecipeGenInfo, RecipeGenInfoAdmin)
+admin.site.register(GenInfo, RecipeGenInfoAdmin)
 
 ###########################################################################################################################
 
-# class RecipeCookingMethodInline(admin.TabularInline):
-#     model = IngredientCookingMethod
-#     extra = 1
+class GenInfoInline(admin.TabularInline):
+    model = GenInfo
+    extra = 1
 
-# class RecipeIngredientAdmin(admin.ModelAdmin):
-#     inline = [RecipeCookingMethodInline]
+class RecipeAdmin(admin.ModelAdmin):
+    inline = [GenInfoInline]
 
-# admin.site.register(RecipeIngredient, RecipeIngredientAdmin)
+admin.site.register(Recipe, RecipeAdmin)
+
--- recipe_info/models.py
+++ recipe_info/models.py
@@ -55,7 +55,15 @@ class CookingMethod(models.Model):
 
     def __str__(self) -> str:
         return f"{self.name_eng}"
-    
+
+class Unit(models.Model):
+    name_eng = models.CharField(max_length=255, unique=True)
+    name_lv = models.CharField(max_length=255, unique=True)
+    name_rus = models.CharField(max_length=255, unique=True)
+    type_shoping_valid = models.BooleanField(null=True)
+    def __str__(self) -> str:
+        return f"{self.name_eng}"
+
 class Ingredient(models.Model):
     # Add price field. Need to find solution to create relation with unit table also
     allergen = models.ForeignKey(Allergen, on_delete=models.SET_NULL, null=True)
@@ -67,42 +75,33 @@ class Ingredient(models.Model):
 
     def __str__(self) -> str:
         return f"{self.name_eng}"
+    
+class Recipe(models.Model):
+    ...
+    # gen_info
+    # ingredient
+    # cooking_method
+    # instruction_steps
 
-class RecipeGenInfo(models.Model):
-    # Move to separated tables
+class GenInfo(models.Model):
+    recipe = models.ForeignKey(Recipe)
     title = models.CharField(max_length=255)
     description = models.TextField()
-
     cuisine = models.ForeignKey(Cuisine, on_delete=models.SET_NULL, null=True)
     meal = models.ForeignKey(Meal, on_delete=models.SET_NULL, null=True)
-    cooking_time = models.PositiveIntegerField()  # in minutes
+    cooking_time = models.PositiveIntegerField()
     servings = models.PositiveIntegerField()
     dietary_preferences = models.ManyToManyField(DietaryPreference)
     equipment = models.ManyToManyField(Equipment)
-    cooking_method = models.ManyToManyField(CookingMethod, through='RecipeCookingMethod')
-    # nutritional_information = models.JSONField(default=dict)
     
     def __str__(self) -> str:
         return f"{self.title}"
-    
-class Unit(models.Model):
-    name_eng = models.CharField(max_length=255, unique=True)
-    name_lv = models.CharField(max_length=255, unique=True)
-    name_rus = models.CharField(max_length=255, unique=True)
-    type_shoping_valid = models.BooleanField(null=True)
-    def __str__(self) -> str:
-        return f"{self.name_eng}"
 
 class RecipeIngredient(models.Model):
-    recipe = models.ForeignKey('RecipeGenInfo', on_delete=models.CASCADE)
-    ingredient = models.ForeignKey('Ingredient', on_delete=models.CASCADE)
+    recipe = models.ForeignKey(GenInfo, on_delete=models.CASCADE)
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
     quantity = models.DecimalField(max_digits=5, decimal_places=2)
     unit = models.ForeignKey(Unit, on_delete=models.CASCADE)
 
     def __str__(self):
         return f"{self.quantity} {self.unit} of {self.ingredient.name_eng}"
-
-class RecipeCookingMethod(models.Model):
-    recipe_data = models.ForeignKey(RecipeGenInfo, on_delete=models.CASCADE)
-    cooking_method = models.ForeignKey(CookingMethod, on_delete=models.CASCADE)
-    unit_count = models.FloatField(null=True)
