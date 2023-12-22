2ab8ae406999d5e36d695d88dda7f0aac1b6c320
Toms <tomsterauds@gmail.com>
2023-12-22 19:46:57 +0200

    complexSearch endpoint added...

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
-from .views import get_recipe, RecipesByIngredientView, get_all_recipes
+from .views import ComplexSearchView, RecipesByIngredientView
 
 urlpatterns = [
-    path('get_all_recipes/', get_all_recipes, name='get_all_recipes'),
-    path('get_recipe/', get_recipe, name='get_recipe'),
-    path('get_recipes_by_ingredient/', RecipesByIngredientView.as_view(), name='get_recipes_by_ingredient'),
+    path('complexSearch/', ComplexSearchView.as_view(), name='complexSearch'),
+    path('findByIngredients/', RecipesByIngredientView.as_view(), name='findByIngredients'),
 ]
--- api/views.py
+++ api/views.py
@@ -1,53 +1,67 @@
-from django.shortcuts import get_object_or_404
-from rest_framework.response import Response
-from rest_framework.decorators import api_view
 from rest_framework.generics import ListAPIView
 
-from recipes.models import CookingStep, Recipe, RecipeIngredient
-from recipes.serializers import CookingStepSerializer, RecipeSerializer
+from recipes.models import Recipe
+from recipes.serializers import RecipeSerializer
 
-@api_view(['GET'])
-def get_all_recipes(request):
-    recipes = Recipe.objects.all()
-    serializer = RecipeSerializer(recipes, many=True)
-    return Response(serializer.data)
+class ComplexSearchView(ListAPIView):
+    serializer_class = RecipeSerializer
 
-@api_view(['GET'])
-def get_recipe(request):
-    recipe_id = request.GET.get('recipe_id')
-    if recipe_id is not None:
-        recipe_instance = get_object_or_404(Recipe, id=recipe_id)
-        cooking_steps_queryset = CookingStep.objects.filter(recipe_id=recipe_id)
+    def get_queryset(self):
+        cuisine_name = self.request.GET.get('cuisine', '')
+        meal_name = self.request.GET.get('meal', '')
+        ingredient_names = self.request.GET.get('ingredients', '').split(',')
+        dietary_preferences = self.request.GET.get('dietary_preferences', '').split(',')
+        equipment_names = self.request.GET.get('equipment', '').split(',')
+        cooking_methods = self.request.GET.get('cooking_methods', '').split(',')
 
-        recipe_serializer = RecipeSerializer(recipe_instance)
-        serialized_recipe_data = recipe_serializer.data
+        if ingredient_names or cuisine_name or meal_name or dietary_preferences or equipment_names or cooking_methods:
+            queryset = Recipe.objects.all()
 
-        cooking_steps_serializer = CookingStepSerializer(cooking_steps_queryset, many=True)
-        serialized_cooking_steps_data = cooking_steps_serializer.data
+            if cuisine_name:
+                queryset = queryset.filter(cuisine__name_eng__icontains=cuisine_name)
+                print(queryset)
+            if meal_name:
+                queryset = queryset.filter(meal__name_eng__icontains=meal_name)
 
-        response_data = {
-            'recipe': serialized_recipe_data,
-            'cooking_steps': serialized_cooking_steps_data,
-        }
+            for ingredient_name in ingredient_names:
+                queryset = queryset.filter(ingredients__ingredient__name_eng__icontains=ingredient_name)
 
-        return Response(response_data)
+            for preference in dietary_preferences:
+                queryset = queryset.filter(dietary_preferences__name_eng__icontains=preference)
 
-    return Response({})
+            for equipment_name in equipment_names:
+                queryset = queryset.filter(equipment__name_eng__icontains=equipment_name)
+
+            for cooking_method in cooking_methods:
+                queryset = queryset.filter(cooking_methods__name_eng__icontains=cooking_method)
+
+            # Add other filters based on additional parameters
+
+            return queryset.distinct()
+
+        return Recipe.objects.none()
+
 
 class RecipesByIngredientView(ListAPIView):
     serializer_class = RecipeSerializer
 
     def get_queryset(self):
-        ingredient_name = self.request.GET.get('ingredient_name')
-        print(f"Ingredient Name: {ingredient_name}")
+        ingredient_names = self.request.GET.get('ingredients', '').split(',')
 
-        if ingredient_name is not None:
-            recipe_ids = RecipeIngredient.objects.filter(ingredient__name_eng__iexact=ingredient_name).values_list('recipe_id', flat=True)
-            print(f"Recipe IDs: {recipe_ids}")
-            queryset = Recipe.objects.filter(id__in=recipe_ids)
-            print(f"Queryset: {queryset}")
-            return queryset
+        if ingredient_names:
+            queryset = Recipe.objects.all()
+            for ingredient_name in ingredient_names:
+                # Use '__ingredient__name_eng__icontains' for case-insensitive search
+                queryset = queryset.filter(ingredients__ingredient__name_eng__icontains=ingredient_name)
+
+            return queryset.distinct()
 
         return Recipe.objects.none()
 
 
+
+# @api_view(['GET'])
+# def get_all_recipes(request):
+#     recipes = Recipe.objects.all()
+#     serializer = RecipeSerializer(recipes, many=True)
+#     return Response(serializer.data)
