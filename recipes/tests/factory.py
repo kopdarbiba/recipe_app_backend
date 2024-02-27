619fafcfb79a7af0767a0913211439a0efe554d8
Toms <tomsterauds@gmail.com>
2024-02-27 14:51:56 +0200

    useless unittests removed

--- recipes/tests/factory.py
+++ recipes/tests/factory.py
@@ -1,11 +1,4 @@
-# tests/factories.py
-import io
-from unittest.mock import patch
 import factory
-from PIL import Image
-from io import BytesIO
-from django.core.files.uploadedfile import SimpleUploadedFile
-from ..models import RecipeImage
 from factory import Factory, SubFactory, Faker
 from ..models import Recipe, Title, Description, Cuisine, Occasion, Meal, DietaryPreference, Equipment, CookingMethod, IngredientCategory, Allergen, CookingMethod, Unit, Ingredient
 
@@ -160,57 +153,3 @@ class RecipeFactory(Factory):
         title = kwargs.get('title') or f'Test Recipe {cls._meta.seq + 1}'
         kwargs['title'] = title
         return super()._create(model_class, *args, **kwargs)
-
-
-# from factory import Factory, SubFactory, post_generation
-# from PIL import Image, ImageOps
-# from recipes.utils.thumbnail_utils import manage_thumbnails
-
-# class RecipeImageFactory(Factory):
-#     class Meta:
-#         model = RecipeImage
-
-#     recipe = SubFactory(RecipeFactory)
-#     image_content = b'\x89PNG\r\n\x1a\n\x00\x00\x00\rIHDR\x00\x00\x01\x00\x00\x00\x01\x00\x08\x06\x00\x00\x00\x1f\x15\xc4\x89\x00\x00\x00\x19tEXtSoftware\x00Adobe ImageReadyq\xc9e<\x00\x00\x03$IDATx\x9c\xec}\t\x94\xe3\xde9\xcfy\xe6\xf9\xf4\xc7\x1c\x87\x84\xbd\x1f\xd3\xb9?\xc9\xfc\x03\x00\x00\xf1zZY\x00\x00\x00\x00IEND\xaeB`\x82'
-#     is_main_image = Faker('boolean')
-
-#     @post_generation
-#     def generate_thumbnail(self, create, extracted, **kwargs):
-#         if not create:
-#             return
-
-#         try:
-#             if self.image_content and not self.thumbnail:
-#                 thumbnail = self.generate_thumbnail()
-
-#                 # Save the thumbnail
-#                 self.save_thumbnail(thumbnail)
-#         except Exception as e:
-#             print(f"Error in generate_thumbnail: {e}")
-
-#     @post_generation
-#     def generate_thumbnail(self):
-#         try:
-#             img = Image.open(io.BytesIO(self.image_content))
-
-#             thumbnail_size = (100, 100)
-#             thumbnail = ImageOps.fit(img, thumbnail_size)
-
-#             return thumbnail
-#         except Exception as e:
-#             print(f"Error in generate_thumbnail: {e}")
-#             return None
-
-#     def save_thumbnail(self, thumbnail):
-#         try:
-#             # Save the thumbnail to the desired location
-#             thumbnail_path = "recipe_images/thumbnails/"  # Adjust this based on your actual structure
-#             thumbnail_name = f"{self.image.name.split('/')[-1].split('.')[0]}_thumbnail.jpg"
-#             thumbnail_full_path = thumbnail_path + thumbnail_name
-#             thumbnail.save(thumbnail_full_path, "JPEG")
-
-#             # Update the thumbnail field in the RecipeImage model
-#             self.thumbnail = thumbnail_full_path
-#             self.save()
-#         except Exception as e:
-#             print(f"Error in save_thumbnail: {e}")
