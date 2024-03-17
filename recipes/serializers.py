a2874aa298f3a9003055c0a09551a273cf6d709c
Toms <tomsterauds@gmail.com>
2024-03-17 17:32:53 +0200

    cuisine, occasion, meal, equipment, cooking methods models added to serializer

--- recipes/serializers.py
+++ recipes/serializers.py
@@ -1,21 +1,6 @@
 from rest_framework import serializers
 
-from .models import CookingStepInstruction, Ingredient, Recipe, RecipeImage, RecipeIngredient, Title, Unit
-
-class ImageSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = RecipeImage
-        fields = ['generate_presigned_url_for_thumbnail', 'generate_presigned_url_for_image']
-
-class CookingStepInstructionSerializer(serializers.ModelSerializer):
-    instruction = serializers.SerializerMethodField()
-    class Meta:
-        model = CookingStepInstruction
-        fields = ['step_number', 'instruction']
-
-    def get_instruction(self, obj) -> str:
-        field_name = f'name_{self.context["lang"]}'
-        return getattr(obj, field_name)
+from .models import CookingMethod, CookingStepInstruction, Equipment, Ingredient, Recipe, RecipeImage, RecipeIngredient, Unit
 
 class IngredientSerializer(serializers.ModelSerializer):
     name = serializers.SerializerMethodField()
@@ -63,18 +48,70 @@ class RecipeIngredientSerializer(serializers.ModelSerializer):
         model = RecipeIngredient
         fields = ['quantity', 'ingredient', 'unit']
 
+class CookingStepInstructionSerializer(serializers.ModelSerializer):
+    instruction = serializers.SerializerMethodField()
+    class Meta:
+        model = CookingStepInstruction
+        fields = ['step_number', 'instruction']
+
+    def get_instruction(self, obj) -> str:
+        field_name = f'name_{self.context["lang"]}'
+        return getattr(obj, field_name)
+
+class ImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = RecipeImage
+        fields = ['generate_presigned_url_for_thumbnail', 'generate_presigned_url_for_image']
+
+class EquipmentSerializer(serializers.ModelSerializer):
+    name = serializers.SerializerMethodField()
+    class Meta:
+        model = Equipment
+        fields = ['name']
+    
+    def get_name(self, obj):
+        if not obj:
+            return None
+        else:
+            field_name = f'name_{self.context["lang"]}'
+            return getattr(obj, field_name)
+
+class CookingMethodSerializer(serializers.ModelSerializer):
+    name = serializers.SerializerMethodField()
+    class Meta:
+        model = CookingMethod
+        fields = ['name']
+    
+    def get_name(self, obj):
+        if not obj:
+            return None
+        else:
+            field_name = f'name_{self.context["lang"]}'
+            return getattr(obj, field_name)
+
 class RecipeSerializer(serializers.ModelSerializer):
-    images = ImageSerializer(many=True)        
-    instructions = CookingStepInstructionSerializer(many=True)
     recipe_ingredients = RecipeIngredientSerializer(many=True)
+    instructions = CookingStepInstructionSerializer(many=True)
+    images = ImageSerializer(many=True)        
+    equipment = EquipmentSerializer(many = True)
+    cooking_methods = CookingMethodSerializer(many = True)
     title = serializers.SerializerMethodField()
     description = serializers.SerializerMethodField()
+    cuisine = serializers.SerializerMethodField()
+    occasion = serializers.SerializerMethodField()
+    meal = serializers.SerializerMethodField()
+    
     class Meta:
         model = Recipe
         fields = [
             'id',
             'title',
             'description',
+            'cuisine',
+            'occasion',
+            'meal',
+            'equipment',
+            'cooking_methods',
             'servings',
             'cooking_time',
             'instructions',
@@ -92,3 +129,12 @@ class RecipeSerializer(serializers.ModelSerializer):
 
     def get_description(self, obj) -> str:
         return self.fetch_lang(obj.description)
+    
+    def get_cuisine(self, obj) -> str:
+        return self.fetch_lang(obj.cuisine)
+
+    def get_occasion(self, obj) -> str:
+        return self.fetch_lang(obj.occasion)
+
+    def get_meal(self, obj) -> str:
+        return self.fetch_lang(obj.meal)
